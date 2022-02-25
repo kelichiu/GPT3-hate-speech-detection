9ec9e023728dec7a979cbeffdd12a679bd6c1472
anniecollins <anneacollins@hotmail.com>
2022-02-25 12:28:22 -0800

    Continued updates to results section

--- DRAFT-misspelling.R
+++ DRAFT-misspelling.R
@@ -6,9 +6,38 @@ one_shot_misspelling <- read.csv(here::here("outputs", "data", "one_shot_misspel
 # Zero-shot
 # Summary of results
 zero_shot_misspelling <- zero_shot_misspelling %>% separate(prompt, c(NA, "comment", NA), sep = "\"")
-zeroshot_summary <- zero_shot_misspelling %>% count(category, comment_id, answer) %>% count(category, comment_id) %>% filter(n > 1)
+zeroshot_summary_race <- zero_shot_misspelling %>% 
+  count(category, comment_id, answer) %>% 
+  count(category, comment_id) %>% 
+  filter(n > 1 & category == "racist")
+zeroshot_summary_sex <- zero_shot_misspelling %>% 
+  count(category, comment_id, answer) %>% 
+  count(category, comment_id) %>% 
+  filter(n > 1 & category == "sexist")
+
+# Racist
+zero_results_summary <- zero_shot_misspelling %>% 
+                          filter(category == "racist" & comment_id %in% zeroshot_summary_race$comment_id) %>% 
+                          filter(comment_id != 26) %>% # 'No,' vs. 'No.' for this comment
+                          arrange(comment_id)
+
+results <- tibble()
+for (i in unique(zero_results_summary$comment_id)) {
+  original <- zero_results_summary %>% filter(comment_id == i & status == "unedited")
+  original_answer <- original["answer"] %>% as.character()
+  edits <- zero_results_summary %>% filter(comment_id == i & status == "edited" & answer != original_answer)
+  results <- rbind(results, original, edits)
+}
+results <- results %>% select(label, status, comment, answer)
+
+
+  
+# Construct summary table
+
+
+# Sexist
 zero_shot_misspelling %>% 
-  filter(category == "racist" & comment_id %in% zeroshot_summary$comment_id)%>% 
+  filter(category == "sexist" & comment_id %in% zeroshot_summary_sex$comment_id)%>% 
   arrange(comment_id) %>%
   View()
 
